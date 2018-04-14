bf0b1ea7a1af4eea3716be4545e709fa2e577ea8
Dan Hemberger <daniel.hemberger@gmail.com>
2018-04-15 01:28:44 +0200

    phpmyadmin: remove config file
    Now that we are no longer using the host mysql, we can use the
    standard docker network link to the mysql container instead, which
    requires no special configuration file.
    
    We are also performing the IP address blocking at the Nginx level,
    so that is not necessary either (and it was burdensome to implement
    as well, since the `proxy_pass` of Nginx caused the wrong IP to be
    sent to the phpmyadmin container).
    
    With no other config options to specify, we remove the config file.

--- phpmyadmin/config.sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Restrict access by IP address
-$i = 1;
-$cfg['Servers'][$i]['host'] = 'localhost';
-$cfg['Servers'][$i]['socket'] = '/var/run/mysqld/mysqld.sock';
-$cfg['Servers'][$i]['AllowDeny']['order'] = 'explicit';
-$cfg['Servers'][$i]['AllowDeny']['rules'] = array('allow % from XX.XXX.XXX.XX');
