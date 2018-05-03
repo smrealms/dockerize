caa433e4a4be0574296fb1dbb7fb0b510f1bc627
Dan Hemberger <daniel.hemberger@gmail.com>
2018-05-03 11:23:23 +0200

    backup_cron.sh: add -T option to docker-compose exec
    Without it, the following error occurs when run with cron:
    
    ValueError: file descriptor cannot be a negative integer (-1)
    
    See https://github.com/docker/compose/issues/3352.

--- backup/backup_cron.sh
+++ backup/backup_cron.sh
@@ -7,7 +7,7 @@ backup_file="$backup_dir/smr_live_$today.sql"
 # cd to root directory of this repository
 cd "$(dirname $0)/.."
 
-docker-compose exec mysql sh -c 'mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --add-drop-table --add-locks --quote-names --databases smr_live' > $backup_file
+docker-compose exec -T mysql sh -c 'mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --add-drop-table --add-locks --quote-names --databases smr_live' > $backup_file
 bzip2 $backup_file
 
 # Keep only the 30 most recent backups
