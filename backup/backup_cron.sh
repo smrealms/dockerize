e65dbfe48aeb9e6129a79369708115b80f4d1228
Dan Hemberger <daniel.hemberger@gmail.com>
2018-11-09 22:25:19 +0100

    backup_cron.sh: make db backups local
    Create the daily db backups locally within the repository so that
    everything (as much as possible) is self-contained.

--- backup/backup_cron.sh
+++ backup/backup_cron.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
-backup_dir="/var/backups/database/daily"
-today=$(date +%Y%m%d)
-backup_file="$backup_dir/smr_live_$today.sql"
-
 # cd to root directory of this repository
 cd "$(dirname $0)/.."
+
+backup_dir="./backup/daily"
+mkdir -p $backup_dir
+
+today=$(date +%Y-%m-%d)
+backup_file="$backup_dir/smr_live_$today.sql"
 
 docker-compose exec -T mysql sh -c 'mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --add-drop-table --add-locks --quote-names --databases smr_live' > $backup_file
 bzip2 $backup_file
