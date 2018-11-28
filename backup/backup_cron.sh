698df9f95c3e69fce7ab72cebfd057f1eb3e043d
Michael Kunze <mkunze@inoxio.de>
2018-11-28 18:11:07 +0100

    Workaround to avoid having the warning message in the dump file
    When mysql is used with -u... -p... a warning message is printed to
    standard out. That message is then piped to the dump file, which makes
    it non sql conform. The password is available on the host in .env and
    inside the container as environment variable. Simply use that to create
    a config file that is used by any client connections.

--- backup/backup_cron.sh
+++ backup/backup_cron.sh
@@ -9,7 +9,11 @@ mkdir -p "${backup_dir}"
 today=$(date +%Y-%m-%d)
 backup_file="$backup_dir/smr_live_$today.sql"
 
-docker-compose exec -T mysql sh -c 'mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --add-drop-table --add-locks --quote-names --databases smr_live' > ${backup_file}
+# Write mysql credentials in the container to avoid
+# mysql: [Warning] Using a password on the command line interface can be insecure.
+docker-compose exec -T mysql sh -c 'echo "[client]\nhost=localhost\nuser=root\npassword=$MYSQL_ROOT_PASSWORD" > ${HOME}/.my.cnf'
+
+docker-compose exec -T mysql sh -c 'mysqldump --add-drop-table --add-locks --quote-names --databases smr_live' > ${backup_file}
 bzip2 "${backup_file}"
 
 # Delete all backups that are older than 14 days
