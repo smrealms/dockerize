3b8e3048ac8a297abf6af5fd78b7875ed1c29966
Dan Hemberger <daniel.hemberger@gmail.com>
2018-12-01 04:02:32 +0100

    get_live_version.sh: add more info
    Print both the live and on-rebuild versions of SMR.
    Add explanatory text describing the output of the script
    along with instructions for how to update the version.

--- get_live_version.sh
+++ get_live_version.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Print the version of the smr code running on the live server
+echo "SMR version in current container (running live server):"
+docker-compose exec smr cat git-commit
+
+echo
+echo "SMR version in current image (will go live on rebuild):"
 docker-compose run --rm smr cat git-commit
+
+echo
+echo 'Run `docker-compose pull smr` to update the image.'
