76c3802d02848fcf7f9b6bb2d16dfa17c9d54b3e
Michael Kunze <mkunze@inoxio.de>
2018-12-03 11:18:15 +0100

    Add a new restore file to be able to import archive databases

---
+++ backup/restore_smr_archive.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null && pwd)"
+cd "${base_dir}" || exit
+
+backup_dir="${base_dir}/backup/archive"
+
+# Take the given parameter as dump or find the youngest backup file in the backup dir
+backup_file="${1:?"Please provide a dump file name."}"
+
+# Check if we have a valid file
+if [[ ! -f "${backup_dir}/${backup_file}" ]]; then
+    echo "Unable to find a valid backup file. Check ${backup_dir}"
+    exit 1
+fi
+
+echo "Import '${backup_file}' into database..."
+
+# Write mysql credentials in the container to avoid
+# mysql: [Warning] Using a password on the command line interface can be insecure.
+docker-compose exec -T mysql sh -c 'echo "[client]\nhost=localhost\nuser=root\npassword=$MYSQL_ROOT_PASSWORD" > ${HOME}/.my.cnf'
+
+# Unzip dump and import on the fly
+bzcat --keep "${backup_dir}/${backup_file}" | docker exec -i smr-mysql mysql
