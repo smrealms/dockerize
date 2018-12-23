6183a9d0541f6be975bd180945ab5c67ec89c718
Dan Hemberger <daniel.hemberger@gmail.com>
2018-12-22 16:55:02 -0800

    Remove nginx configuration files
    We have replaced nginx with traefik, so the nginx configuration
    files are no longer needed.

--- install_certs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# This script should only be used for the initial install of
-# the SSL certificates with certbot and nginx.
-docker-compose up --build -d nginx-certbot-install
-docker-compose run --rm certbot
-docker-compose stop nginx-certbot-install
