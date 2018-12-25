4bfdcf6874173c42fdfec792aa20da5482cf6584
Michael Kunze <mkunze@inoxio.de>
2018-12-25 15:09:13 +0100

    Make docker-compose call explicit
    When running as cron-job, /usr/local/bin is not on the path. To fix that
    use an absolute path to call docker-compose

--- backup/backup_cron.sh
+++ backup/backup_cron.sh
@@ -11,9 +11,9 @@ backup_file="$backup_dir/smr_live_$today.sql"
 
 # Write mysql credentials in the container to avoid
 # mysql: [Warning] Using a password on the command line interface can be insecure.
-docker-compose exec -T mysql sh -c 'echo "[client]\nhost=localhost\nuser=root\npassword=$MYSQL_ROOT_PASSWORD" > ${HOME}/.my.cnf'
+/usr/local/bin/docker-compose exec -T mysql sh -c 'echo "[client]\nhost=localhost\nuser=root\npassword=$MYSQL_ROOT_PASSWORD" > ${HOME}/.my.cnf'
 
-docker-compose exec -T mysql sh -c 'mysqldump --add-drop-table --add-locks --quote-names --databases smr_live' > ${backup_file}
+/usr/local/bin/docker-compose exec -T mysql sh -c 'mysqldump --add-drop-table --add-locks --quote-names --databases smr_live' > ${backup_file}
 bzip2 "${backup_file}"
 
 # Delete all backups that are older than 14 days
