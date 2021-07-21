572d96f999b6d0d9695a00219a5ba5719794a466
Dan Hemberger <daniel.hemberger@gmail.com>
2021-07-21 07:00:28 -0700

    get_live_version.sh: update smr image label
    The `smrealms/smr` label for the main web service was changed from
    `latest` to `web` in bae2f36d1.

--- get_live_version.sh
+++ get_live_version.sh
@@ -5,7 +5,7 @@ docker-compose exec smr cat git-commit
 
 echo
 echo "SMR version in latest image (will go live on rebuild):"
-docker run --rm smrealms/smr cat git-commit
+docker run --rm smrealms/smr:web cat git-commit
 
 echo
 echo 'Run `docker-compose up -d smr` to update the live server.'
