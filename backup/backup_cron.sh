6e13bf9a6fb333c49885b36714affcc420b7e77a
Dan Hemberger <daniel.hemberger@gmail.com>
2022-08-01 06:32:55 -0700

    backup_cron.sh: remove writing .my.cnf
    The `smr_live` ansible role is responsible for writing the .my.cnf
    file, so we should omit the duplication here.

--- backup/backup_cron.sh
+++ backup/backup_cron.sh
@@ -9,10 +9,6 @@ mkdir -p "${backup_dir}"
 today=$(date +%Y-%m-%d)
 backup_file="$backup_dir/smr_live_$today.sql"
 
-# Write mysql credentials in the container to avoid
-# mysql: [Warning] Using a password on the command line interface can be insecure.
-/usr/local/bin/docker-compose exec -T mysql sh -c 'echo "[client]\nhost=localhost\nuser=root\npassword=$MYSQL_ROOT_PASSWORD" > ${HOME}/.my.cnf'
-
 /usr/local/bin/docker-compose exec -T mysql sh -c 'mysqldump --add-drop-table --add-locks --quote-names --databases smr_live' > ${backup_file}
 bzip2 "${backup_file}"
 
